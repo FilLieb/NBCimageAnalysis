10edd0f5eb3b206598ba0364f9b5681698b190c1
FilLieb <f.liebsch@uni-koeln.de>
2025-04-24 09:08:14 +0200

    fixed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import importlib.metadata, subprocess, sys
 #'matplotlip==3.10.1'
-required  = {'aicsimageio', 'aicsimageio[nd2]'}
+required  = {'aicsimageio', 'aicsimageio[nd2]', 'scikit-image'}
 installed = {pkg.metadata['Name'] for pkg in importlib.metadata.distributions()}
 missing   = required - installed
 
@@ -9,9 +9,9 @@ if missing:
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
 
 
-
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 from aicsimageio import AICSImage
+from skimage.io import imshow
 
 img = AICSImage('data/WT_001.nd2')
 data = img.get_image_data("TCZYX")  # Choose the correct dimension order
@@ -22,21 +22,32 @@ t_index = 0
 z_index = 0
 
 # Extract each channel slice (assume 3 channels, for example)
-#channel_0 = data[t_index, 0, z_index, :, :]
-#channel_1 = data[t_index, 1, z_index, :, :]
-#channel_2 = data[t_index, 2, z_index, :, :]
+channel_0 = data[t_index, 0, z_index, :, :]
+channel_1 = data[t_index, 1, z_index, :, :]
+channel_2 = data[t_index, 2, z_index, :, :]
 
 
-#fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+fig, axes = plt.subplots(1, 3, figsize=(12, 4))
 
-#axes[0].imshow(channel_0, cmap='gray')
-#axes[0].set_title("Channel 0")
+axes[0].imshow(channel_0, cmap='gray')
+axes[0].set_title("Channel 0")
 
-#axes[1].imshow(channel_1, cmap='gray')
-#axes[1].set_title("Channel 1")
+axes[1].imshow(channel_1, cmap='gray')
+axes[1].set_title("Channel 1")
 
-#axes[2].imshow(channel_2, cmap='gray')
-#axes[2].set_title("Channel 2")
+axes[2].imshow(channel_2, cmap='gray')
+axes[2].set_title("Channel 2")
 
-#plt.tight_layout()
-#plt.show()
+plt.tight_layout()
+plt.show()
+
+
+def get_voxel_size_from_aics_image(aics_image):
+    return (aics_image.physical_pixel_sizes.Z,
+            aics_image.physical_pixel_sizes.Y,
+            aics_image.physical_pixel_sizes.X)
+
+
+voxel = get_voxel_size_from_aics_image(img)
+
+print(voxel)
